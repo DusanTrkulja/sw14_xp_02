08bd675752c679c351af70bae8692aa2d454306a
mstrasser92 <m.strasser@student.tugraz.at>
2014-04-02 09:53:19 +0200

    GUI tests für Login

---
+++ test/src/com/github/sw14_xp_02/test/GUItestLogin.java
@@ -0,0 +1,39 @@
+package com.github.sw14_xp_02.test;
+
+import com.github.sw14_xp_02.LoginActivity;
+import com.robotium.solo.Solo;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class GUItestLogin extends ActivityInstrumentationTestCase2<LoginActivity> {
+
+	private Solo LoginSolo;
+	
+	public GUItestLogin(String LoginActivity) {
+		super(LoginActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		LoginSolo = new Solo(getInstrumentation(), getActivity());
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testButtons(){
+		LoginSolo.clickOnButton("Login");
+		LoginSolo.clickOnButton("Register");
+	}
+	
+	public void testInputFields() {
+		LoginSolo.clickOnEditText(0);
+		LoginSolo.typeText(0, "email@test.com");
+		LoginSolo.getText("email@test.com");
+		LoginSolo.clickOnEditText(1);
+		LoginSolo.typeText(1, "password");
+		LoginSolo.getText("password");
+	}
+
+}
