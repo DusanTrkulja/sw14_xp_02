e761bce69b04bae7468fa85f2eda6f8fd3450595
mstrasser92 <m.strasser@student.tugraz.at>
2014-04-09 11:38:19 +0200

    [MS, AP] {testing} ChatTests added

--- ChatTest/src/at/sw_xp_02/whisper/test/MainActivityTest.java
+++ ChatTest/src/at/sw_xp_02/whisper/test/MainActivityTest.java
@@ -1,8 +1,11 @@
 package at.sw_xp_02.whisper.test;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+import at.sw_xp_02.whisper.ChatActivity;
 import at.sw_xp_02.whisper.MainActivity;
 import at.sw_xp_02.whisper.SettingsActivity;
+import at.sw_xp_02.whisper.R;
 
 import com.robotium.solo.Solo;
 
@@ -10,6 +13,7 @@ public class MainActivityTest extends
 ActivityInstrumentationTestCase2<MainActivity> {
 
 	private Solo solo;
+	private EditText message;
 
 	public MainActivityTest() {
 		super(MainActivity.class);
@@ -17,6 +21,10 @@ ActivityInstrumentationTestCase2<MainActivity> {
 
 	public void setUp() throws Exception {
 		solo = new Solo(getInstrumentation(), getActivity());
+//		solo.clickOnActionBarItem(R.id.action_add);
+//		solo.typeText(0, "strass.michael@gmx.net");
+//		solo.clickOnButton(1);
+//		solo.clickOnText("strass.michael@gmx.net");
 	}
 
 	public void testPreferenceActivity() {
@@ -27,6 +35,17 @@ ActivityInstrumentationTestCase2<MainActivity> {
 
 	}
 
+	public void testSendMessage() throws InterruptedException {
+		solo.clickOnText("strass.michael@gmx.net");
+		solo.assertCurrentActivity("ChatActivity", ChatActivity.class);
+		message = (EditText) solo.getView(R.id.msg_edit);
+		solo.typeText(0, "Test Message");
+		solo.clickOnButton("Send");
+		solo.sleep(500);
+		solo.waitForText("Test Message");
+		assertEquals("", message.getText());
+		
+	}
 
 	@Override
 	public void tearDown() throws Exception {
