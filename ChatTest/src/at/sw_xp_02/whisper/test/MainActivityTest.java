a44880603eae11dc74be4a3654475d1713ea9410
Alexander Pann <alexander.pann@gmail.com>
2014-04-09 14:05:04 +0200

    [MS,AP]{test after code refactoring} - Tests for offline message sending added + minor refactoring.

--- ChatTest/src/at/sw_xp_02/whisper/test/MainActivityTest.java
+++ ChatTest/src/at/sw_xp_02/whisper/test/MainActivityTest.java
@@ -1,5 +1,6 @@
 package at.sw_xp_02.whisper.test;
 
+import android.support.v4.app.Fragment;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.EditText;
 import at.sw_xp_02.whisper.ChatActivity;
@@ -14,6 +15,7 @@ ActivityInstrumentationTestCase2<MainActivity> {
 
 	private Solo solo;
 	private EditText message;
+	private android.app.Fragment fragment;
 
 	public MainActivityTest() {
 		super(MainActivity.class);
@@ -43,8 +45,17 @@ ActivityInstrumentationTestCase2<MainActivity> {
 		solo.clickOnButton("Send");
 		solo.sleep(500);
 		solo.waitForText("Test Message");
-		assertEquals("", message.getText());
+		assertTrue(message.getText().length()== 0);
 		
+	}
+	
+	public void testOfflineMessageFailure() {
+		solo.clickOnText("strass.michael@gmx.net");
+		solo.assertCurrentActivity("ChatActivity", ChatActivity.class);
+		message = (EditText) solo.getView(R.id.msg_edit);
+		solo.typeText(0, "Test Message");
+		solo.clickOnButton("Send");
+		solo.waitForText("Message could not be sent");
 	}
 
 	@Override
