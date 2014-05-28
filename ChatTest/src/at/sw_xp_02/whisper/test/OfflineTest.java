4a39560931f86b81ca5fd77b7280ed5620bd2c9b
mstrasser92 <m.strasser@student.tugraz.at>
2014-05-28 12:07:51 +0200

    [JE, MS] {testing} Test if message not send if you are offline, added turning wifi and data automatically off

--- ChatTest/src/at/sw_xp_02/whisper/test/OfflineTest.java
+++ ChatTest/src/at/sw_xp_02/whisper/test/OfflineTest.java
@@ -1,22 +1,18 @@
 package at.sw_xp_02.whisper.test;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
 import android.net.wifi.WifiManager;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.EditText;
-import android.widget.ListView;
 import at.sw_xp_02.whisper.ChatActivity;
-import at.sw_xp_02.whisper.Common;
-import at.sw_xp_02.whisper.DataProvider;
 import at.sw_xp_02.whisper.MainActivity;
-import at.sw_xp_02.whisper.SettingsActivity;
 import at.sw_xp_02.whisper.R;
-import at.sw_xp_02.whisper.client.ServerUtilities;
+
 
 import com.robotium.solo.Solo;
 
@@ -35,8 +31,19 @@ ActivityInstrumentationTestCase2<MainActivity> {
 		
 		// todo disconect wifi
 		turnWifi(false);
-
+		setMobileDataEnabled(getActivity(),false);
+		solo.goBackToActivity("MainActivity");
 	}
+	
+//	private void enableDebugMode(Boolean enable) {
+//		solo.goBackToActivity("MainActivity"); 
+//		solo.clickOnActionBarItem(R.id.action_add);
+//		 if(enable)
+//			 solo.typeText(0, "#debug*!");
+//		 else
+//			 solo.typeText(0, "#nodebug*!");
+//     solo.clickOnButton(1);
+//	}
 	
 	private void addDummyUser(String email) {
 	    solo.clickOnActionBarItem(R.id.action_add);
@@ -44,11 +51,30 @@ ActivityInstrumentationTestCase2<MainActivity> {
         solo.clickOnButton(1);
 	}
 	
+	private void setMobileDataEnabled(Context context, boolean enabled) {
+	
+		try {
+			final ConnectivityManager conman = (ConnectivityManager)  context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		  final Class conmanClass = Class.forName(conman.getClass().getName());
+		  final Field iConnectivityManagerField = conmanClass.getDeclaredField("mService");
+		  iConnectivityManagerField.setAccessible(true);
+		  final Object iConnectivityManager = iConnectivityManagerField.get(conman);
+		  final Class iConnectivityManagerClass = Class.forName(iConnectivityManager.getClass().getName());
+		  final Method setMobileDataEnabledMethod = iConnectivityManagerClass.getDeclaredMethod("setMobileDataEnabled", Boolean.TYPE);
+		  setMobileDataEnabledMethod.setAccessible(true);
+		  setMobileDataEnabledMethod.invoke(iConnectivityManager, enabled);
+		} catch (Exception ignored) {
+			// TODO Auto-generated catch block
+		}
+	  
+	}
+	
 	protected void turnWifi(boolean enabled) {
 	    try {
 	        WifiManager wifiManager = (WifiManager) getInstrumentation()
 	                .getTargetContext().getSystemService(Context.WIFI_SERVICE);
 	        wifiManager.setWifiEnabled(enabled);
+	        
 	    } catch (Exception ignored) {
 	        // don't interrupt test execution, if there
 	        // is no permission for that action
@@ -63,8 +89,17 @@ ActivityInstrumentationTestCase2<MainActivity> {
 		message = (EditText) solo.getView(R.id.msg_edit);
 		solo.typeText(0, "Test Message");
 		solo.clickOnButton("Send");
-		solo.sleep(5000);
-		solo.getText("Message could not be sent");
+		solo.waitForText("Message could not be sent");
 	}
 	
+	@Override
+	public void tearDown() throws Exception {
+		solo.finishOpenedActivities();
+		
+		setMobileDataEnabled(getActivity(),true);
+
+		// todo connect wifi
+		turnWifi(true);
+    
+	}
 }
